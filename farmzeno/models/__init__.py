f348789c1fc217926bf1e63a2d7c25e4e5cb26ea
Kotagiri kulbhushan <kulbhushankotagiri@gmail.com>
2026-04-18 17:00:35 +0530

    Removed user-generated files and updated .gitignore

--- farmzeno/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# models/__init__.py
-from models.disease_model import load_disease_model, predict_disease
-from models.crop_model import load_crop_model, predict_crop, model_loaded
-
-__all__ = [
-    "load_disease_model", "predict_disease",
-    "load_crop_model", "predict_crop", "model_loaded",
-]
